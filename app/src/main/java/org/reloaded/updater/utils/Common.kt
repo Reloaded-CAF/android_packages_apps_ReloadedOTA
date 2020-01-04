622e3d3cbfcf4fb87cf1f01b3c7ea65d925cd666
Ritesh Saxena <riteshsax007@gmail.com>
2020-01-04 18:47:44 +0530

    ota: fix crash due to date format

--- app/src/main/java/org/reloaded/updater/utils/Common.kt
+++ app/src/main/java/org/reloaded/updater/utils/Common.kt
@@ -2,30 +2,38 @@ package org.reloaded.updater.utils
 
 import android.content.Context
 import android.net.ConnectivityManager
+import android.os.Build
 import java.text.SimpleDateFormat
+import java.util.Date
 
 class Common {
     companion object {
-        fun checkNetwork(context: Context): Boolean{
-            val networkInfo = (context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager).activeNetworkInfo
+        fun checkNetwork(context: Context): Boolean {
+            val networkInfo =
+                (context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager).activeNetworkInfo
             return networkInfo != null && networkInfo.isConnected
         }
 
-        fun getBuildDate(context: Context) : String{
-            val date = SystemPropertiesProxy[context, "ro.build.date"]
-            val format = SimpleDateFormat("E MMM dd HH:mm:ss Z yyyy")
-            val dateObj = format.parse(date)
-            val simpleDateFormat = SimpleDateFormat("d MMM yyyy")
-
-            return simpleDateFormat.format(dateObj)
+        fun getBuildDate(context: Context): String {
+            val version = this.getVersion(context)
+            val outputFormat = SimpleDateFormat("d MMM yyyy")
+            if (version.equals("unknown")) {
+                val dateObj = Date(Build.TIME)
+                return outputFormat.format(dateObj)
+            } else {
+                val buildDate = version.split("-")[3]
+                val format = SimpleDateFormat("yyyyMMdd")
+                val dateObj = format.parse(buildDate)
+                return outputFormat.format(dateObj)
+            }
         }
 
-        fun getVersion(context: Context) : String{
-            return SystemPropertiesProxy[context, "ro.reloaded.version","unknown"]
+        fun getVersion(context: Context): String {
+            return SystemPropertiesProxy[context, "ro.reloaded.version", "unknown"]
         }
 
-        fun getDevice(context: Context) : String{
-            return SystemPropertiesProxy[context, "ro.reloaded.device","unknown"]
+        fun getDevice(context: Context): String {
+            return SystemPropertiesProxy[context, "ro.reloaded.device", "unknown"]
         }
     }
 }
